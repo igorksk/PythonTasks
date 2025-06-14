32555af05281ed1e9ec3777a2d7dc8a591e391f3
Igor Kaseko <igorksk@gmail.com>
2025-06-14 22:42:10 +0300

    compare lists

---
+++ book_chapter2/compare_int_lists.py
@@ -0,0 +1,17 @@
+def compare(list1, list2):
+
+    if (len(list1) != len(list2)):
+        return False;
+
+    for i in range(len(list1)):
+        for j in range(len(list2)):
+            if i == j and list1[i] != list2[j]:
+                return False
+
+    return True;
+
+
+print(compare([1, 2, 3], [1, 2, 3])) # True
+print(compare([1, 2, 3], [3, 2, 1])) # False 
+print(compare([1, 2, 3], [1, 2]))  # False
+
