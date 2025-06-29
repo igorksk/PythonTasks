42bc93e3ac3b40d93afb3c528e571d9b23ae356b
Igor Kaseko <igorksk@gmail.com>
2025-06-29 11:37:28 +0300

    day of week

---
+++ book_chapter2/8.day_of_week.py
@@ -0,0 +1,20 @@
+# Ask the user to enter a number
+number = int(input("Enter a number from 1 to 7: "))
+
+# List of day names
+days = [
+    "Monday",    # 1
+    "Tuesday",   # 2
+    "Wednesday", # 3
+    "Thursday",  # 4
+    "Friday",    # 5
+    "Saturday",  # 6
+    "Sunday"     # 7
+]
+
+# Check if the number is in the valid range
+if 1 <= number <= 7:
+    day_name = days[number - 1]
+    print("Day of the week:", day_name)
+else:
+    print("Invalid number. Please enter a number from 1 to 7.")
